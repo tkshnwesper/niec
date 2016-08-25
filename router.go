9b3867ccf01d8f5a7ef790f9c7f6230a13273504
Saurabh <unknown7bolt@gmail.com>
2016-08-25 13:18:21 +0530

    added semantic and gitignore

--- router.go
+++ router.go
@@ -2,10 +2,21 @@ package main
 
 import (
     "github.com/kataras/iris"
+    "github.com/iris-contrib/template/html"
 )
 
 func RouterInit() {
+    // iris.StaticServe("./static/", "static")
+    
+    iris.UseTemplate(html.New(html.Config {
+        Layout: "default-layout.html",
+    }))
+    
     iris.Get("/", func(c *iris.Context) {
-        c.Write("Hello")
+        c.Render("index.html", struct{
+            Title string
+        }{
+            "Hello world!",
+        })
     })
 }
