b6bdaa427d7fb7f17ef162266bcc234fe8a2729a
Saurabh <unknown7bolt@gmail.com>
2016-09-05 14:18:23 +0530

    fixed url issues

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"github.com/kataras/iris"
+	// "github.com/kataras/iris/config"
 )
 
 func main() {
+	// def := config.DefaultServer()
+	// man := config.Server {
+	// 	Virtual: true,
+	// 	VListeningAddr: ":80",
+	// 	ListeningAddr: "",
+	// 	VScheme: "/",
+	// }
+	// conf := def.MergeSingle(man)
+	// iris.Servers.Add(conf)
 	RouterInit()
 	InitDB()
-	iris.Listen("192.168.1.2:8081")
+	iris.Listen(":8081")
+	// iris.Go()
+	
 }
